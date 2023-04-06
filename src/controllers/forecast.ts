a68bdedeb18b907ee5ce18b36c40f9d82fd684cc
WildBarreto <wildsbsrreto@gmail.com>
2023-04-06 16:30:44 -0300

    Integrando Beach e Forecast e mockando APIs com nock

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,19 +1,23 @@
 import { Controller, Get } from '@overnightjs/core';
+import { Request, Response } from 'express';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
-import { Request, Response } from 'express';
 
 const forecast = new Forecast();
 
 @Controller('forecast')
 export class ForecastController {
   @Get('')
-  public async getForecastForLoggedUser(
+  public async getForecastForgeLoggedUser(
     _: Request,
     res: Response
   ): Promise<void> {
-    const beaches = await Beach.find({});
-    const forecastData = await forecast.processForecastForBeaches(beaches);
-    res.status(200).send(forecastData)
+    try {
+      const beaches = await Beach.find({});
+      const forecastData = await forecast.processForecastForBeaches(beaches);
+      res.status(200).send(forecastData);
+    } catch (error) {
+      res.status(500).send({ error: 'Something went wrong' });
+    }
   }
 }
